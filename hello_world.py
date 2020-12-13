9224d34ac900071efee0c9b40f5f7732b6e4fd03
HonzikChoutka <ajchoutka@gmail.com>
2020-12-13 11:27:06 -0700

    In progress hello world

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,15 @@
-print("Hello World")
-asbgvouiqeabrougv
+
+_list = []
+_list2 = []
+
+exclamation = "!"
+typo = "World"
+tipo = "Hello"
+
+def hello_world(times):
+    for letter in tipo:
+         _list += letter
+    for letters in typo:
+          _list2 += letters
+          
+print("Hello World!")
