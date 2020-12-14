be0ae1afde849902f56a8170f8e1532a994f3522
HonzikChoutka <ajchoutka@gmail.com>
2020-12-13 17:24:03 -0700

    Added ..py

--- hello_world.py
+++ hello_world.py
@@ -1,23 +1,2 @@
-
-list1 = []
-list2 = []
-
-exclamation = "!"
-typo = "World"
-tipo = "Hello"
-
-def hello_world():
-    
-    list1 = []
-    list2 = []
-
-    for letter in tipo:
-        list1 += letter
-    for letters in typo:
-        list2 += letters
-    join(list1, list2)
-    
-hello_world()
-          
 print("Hello World!")
-print(list1)
+print("Goodbye")
